c419e0d21988fab68dffd04c0ddc2513964ee2a9
premjavasoft <157012416+premjavasoft@users.noreply.github.com>
2024-01-18 16:18:27 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+package com.epam.learning;
+public class Test {
+
+	public static void main(String[] args) {
+		//System.out.println("HI");
+		int a=10;
+		
+		if(a==10) {
+			System.out.print("if");
+		}
+		else if(a==10) {
+			System.out.print("else if");
+		}
+		else {
+			System.out.print("else");
+		}
+
+	}
+
+}
